5c6161e61fcb09d30e866b9dbdd9c421eec8beef
nikeshsakthi <jsakthinikesh@gmail.com>
2024-08-09 00:53:41 +0530

    Upated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#008080", // Teal
+        "secondary": "#e0f7fa", // Light Teal
+        "lightBg": "#e0f2f1", // Light Teal Background
+        "lightSecondary": "#ffffff", // White
+        "primaryText": "#004d4d", // Dark Teal Text
+        "secondaryText": "#007373" // Medium Teal Text
+      },
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
+
